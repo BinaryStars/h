f3573ae969f636c879f9a027b8e2dff3becb4180
Nick Stenning <nick@whiteink.com>
2016-09-30 16:44:01 +0200

    Move activity views into h.views package
    As part of an effort to move all views into the h.views package, this
    moves the activity views out of h.activity and into h.views.

--- h/app.py
+++ h/app.py
@@ -121,7 +121,6 @@ def includeme(config):
     # Site modules
     config.include('h.accounts')
     config.include('h.accounts.views')
-    config.include('h.activity')
     config.include('h.admin')
     config.include('h.badge')
     config.include('h.feeds')
